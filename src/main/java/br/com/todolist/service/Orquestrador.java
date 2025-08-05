b5c8bfef34a8957e751a6bf47e6d36d3df91f987
Rickson-Costa <ricksonboy10@gmail.com>
2025-08-05 13:11:56 -0300

    Inclusão da chamada dos métodos de envio de email na classe Orquestrador!

--- src/main/java/br/com/todolist/service/Orquestrador.java
+++ src/main/java/br/com/todolist/service/Orquestrador.java
@@ -1,9 +1,9 @@
-// Em: src/main/java/br/com/todolist/service/Orquestrador.java
 package br.com.todolist.service;
 
 import br.com.todolist.models.Evento;
 import br.com.todolist.models.Tarefa;
 import br.com.todolist.models.Usuario;
+import br.com.todolist.util.Mensageiro;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
@@ -13,17 +13,25 @@ public class Orquestrador {
     private final GerenteDeTarefas gerenteDeTarefas;
     private final GerenteDeEventos gerenteDeEventos;
     private final String emailUsuario;
+    private final Mensageiro mensageiro;
 
     public Orquestrador(Usuario usuario) {
         GerenteDeDadosDoUsuario dadosDoUsuario = new GerenteDeDadosDoUsuario();
         this.emailUsuario = usuario.getEmail();
         this.gerenteDeTarefas = new GerenteDeTarefas(dadosDoUsuario, this.emailUsuario);
         this.gerenteDeEventos = new GerenteDeEventos(dadosDoUsuario, this.emailUsuario);
+        this.mensageiro = new Mensageiro();
     }
 
     public void cadastrarTarefa(String titulo, String descricao, LocalDate deadline, int prioridade) {
         Tarefa novaTarefa = new Tarefa(titulo, descricao, this.emailUsuario, deadline, prioridade);
         this.gerenteDeTarefas.cadastrarTarefa(novaTarefa);
+        String assunto = "Notificação: Nova Tarefa Criada";
+        String corpo = "Uma nova tarefa foi criada:\n\n"
+                + "Título: " + titulo + "\n"
+                + "Descrição: " + descricao + "\n"
+                + "Prazo: " + deadline + "\n";
+        mensageiro.enviarEmail(this.emailUsuario, assunto, corpo);
     }
 
     public List<Tarefa> listarTodasTarefas() {
@@ -32,11 +40,17 @@ public class Orquestrador {
 
     public void excluirTarefa(Tarefa tarefa) {
         this.gerenteDeTarefas.excluirTarefa(tarefa);
+        String assunto = "Notificação: Tarefa Excluída";
+        String corpo = "A tarefa '" + tarefa.getTitulo() + "' foi excluída.";
+        mensageiro.enviarEmail(this.emailUsuario, assunto, corpo);
     }
 
     public void editarTarefa(Tarefa tarefaOriginal, String novoTitulo, String novaDescricao, LocalDate novoDeadline,
             int novaPrioridade) {
         this.gerenteDeTarefas.editarTarefa(tarefaOriginal, novoTitulo, novaDescricao, novoDeadline, novaPrioridade);
+        String assunto = "Notificação: Tarefa Editada";
+        String corpo = "A tarefa '" + tarefaOriginal.getTitulo() + "' foi editada.";
+        mensageiro.enviarEmail(this.emailUsuario, assunto, corpo);
     }
 
     public void atualizarTarefa(Tarefa tarefa) {
@@ -52,8 +66,17 @@ public class Orquestrador {
     }
 
     public boolean cadastrarEvento(String titulo, String descricao, LocalDate deadline) {
-        Evento novoEvento = new Evento(titulo, descricao, this.emailUsuario, deadline);
-        return this.gerenteDeEventos.cadastrarEvento(novoEvento);
+        boolean sucesso = this.gerenteDeEventos
+                .cadastrarEvento(new Evento(titulo, descricao, this.emailUsuario, deadline));
+        if (sucesso) {
+            String assunto = "Notificação: Novo Evento Criado";
+            String corpo = "Um novo evento foi criado:\n\n"
+                    + "Título: " + titulo + "\n"
+                    + "Descrição: " + descricao + "\n"
+                    + "Prazo: " + deadline + "\n";
+            mensageiro.enviarEmail(this.emailUsuario, assunto, corpo);
+        }
+        return sucesso;
     }
 
     public List<Evento> listarTodosEventos() {
@@ -62,10 +85,16 @@ public class Orquestrador {
 
     public void excluirEvento(Evento evento) {
         this.gerenteDeEventos.excluirEvento(evento);
+        String assunto = "Notificação: Evento Excluído";
+        String corpo = "O evento '" + evento.getTitulo() + "' foi excluído.";
+        mensageiro.enviarEmail(this.emailUsuario, assunto, corpo);
     }
 
     public void editarEvento(Evento eventoOriginal, String novoTitulo, String novaDescricao, LocalDate novoDeadline) {
         this.gerenteDeEventos.editarEvento(eventoOriginal, novoTitulo, novaDescricao, novoDeadline);
+        String assunto = "Notificação: Evento Editado";
+        String corpo = "O evento '" + eventoOriginal.getTitulo() + "' foi editado.";
+        mensageiro.enviarEmail(this.emailUsuario, assunto, corpo);
     }
 
     public List<Evento> listarEventosPorDia(LocalDate dia) {
